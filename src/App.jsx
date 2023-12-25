185bc75dc6b5eef8fdf34a8a77cb1c4bc01f100e
AlexanderSupertramp <alexandresamxaradze@Alexandres-Mac-mini.local>
2023-12-26 01:43:48 +0400

    replaced svg animation, with component

--- src/App.jsx
+++ src/App.jsx
@@ -196,43 +196,13 @@ function Formv1() {
                 }}
             >
                 {loading ? (
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        className='h-full fill-white'
-                    >
-                        <circle cx="4" cy="12" r="3" opacity="1">
-                            <animate
-                                id="spinner_qYjJ"
-                                begin="0;spinner_t4KZ.end-0.25s"
-                                attributeName="opacity"
-                                dur="0.75s"
-                                values="1;.2"
-                                fill="freeze"
-                            />
-                        </circle>
-                        <circle cx="12" cy="12" r="3" opacity=".4">
-                            <animate
-                                begin="spinner_qYjJ.begin+0.15s"
-                                attributeName="opacity"
-                                dur="0.75s"
-                                values="1;.2"
-                                fill="freeze"
-                            />
-                        </circle>
-                        <circle cx="20" cy="12" r="3" opacity=".3">
-                            <animate
-                                id="spinner_t4KZ"
-                                begin="spinner_qYjJ.begin+0.3s"
-                                attributeName="opacity"
-                                dur="0.75s"
-                                values="1;.2"
-                                fill="freeze"
-                            />
-                        </circle>
-                    </svg>
+                        <l-waveform
+                          size="18"
+                          speed="1"
+                          stroke="2"
+                          color={iconColor}
+                          className='h-full fill-white'
+                        ></l-waveform>
                 ) : submitted ? (
                     <div className='flex items-center justify-center'>
 
